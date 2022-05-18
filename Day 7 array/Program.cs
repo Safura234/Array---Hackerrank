006ee8676d10ebffaf1401b0a0020048f2623059
Safura234 <safuramg@code.edu.az>
2022-05-18 17:18:04 +0400

    https://github.com/Safura234/Array---Hackerrank.git

---
+++ Day 7 array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_7_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
